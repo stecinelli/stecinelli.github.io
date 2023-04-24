4c42cdbb920f850fc4454b0dd23f016141bfee63
Stephanie Cinelli <stephaniecinelli@gmail.com>
2023-04-24 11:22:37 +0200

    initial commit

---
+++ client/src/Components/AppBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MuiAppBar, { AppBarProps } from '@mui/material/AppBar';
+
+
+const AppBar = (props: AppBarProps) => {
+  return (
+    <MuiAppBar elevation={0} position="fixed" {...props} />
+  )
+}
+
+export default AppBar
